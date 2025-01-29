003dfbf9ce26058c424db8e97f200c44ea6f8bc3
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-01-29 17:13:12 +0500

    Front Finish

--- src/components/weather-components/index.tsx
+++ src/components/weather-components/index.tsx
@@ -8,6 +8,14 @@ import normaly from "../../img/normaly.svg";
 import loop from "../../img/loop.svg";
 
 const WeatherComponents = () => {
+  // Style Bular
+  const divStyle = "flex justify-between items-center ";
+  const divStyle2 = "flex items-center gap-[30px]";
+  const h429 = "text-[rgb(197,206,210)] text-[19px] font-normal";
+  const p29 = "text-[19px] text-[#FFF]";
+  const imgWith = "w-[17px]";
+  // Style Bular
+
   return (
     <section className="flex justify-between w-full h-[100vh]">
       <div className="pt-4 flex flex-col justify-between">
@@ -15,7 +23,7 @@ const WeatherComponents = () => {
           <img className="w-[200px] top-0" src={imgLogo} alt="" />
         </div>
         <div className="flex gap-[25px] items-center p-[70px]">
-          <h1 className="text-[#FFF] text-[92px] font-bold">19* </h1>
+          <h1 className="text-[#FFF] text-[92px] font-bold">19 °C </h1>
           <div className="flex gap-2 items-center ">
             <div className="">
               <h2 className="text-[#FFF] text-[36px] font-bold">Andijan</h2>
@@ -44,48 +52,54 @@ const WeatherComponents = () => {
             </button>
           </form>
 
-          <div>
-            <h3>Ob-havo tafsilotlari...</h3>
+          <div className="py-[60px]">
+            <h3 className="text-[#FFF] text-[19px] font-bold ">
+              Ob-havo tafsilotlari...
+            </h3>
           </div>
 
-          <div>
-            <h2>Yengil yomg'irli bo'ron</h2>
+          <div className="pb-[40px]">
+            <h2 className="text-[#FFF] text-[19px] font-medium font-mono">
+              Yengil yomg'irli bo'ron
+            </h2>
           </div>
 
-          <div>
-            <div>
-              <h4>Joriy harorat</h4>
-              <div>
-                <p>19 °C</p>
-                <img src={maxTemp} alt="" />
+          <div className="flex flex-col gap-9">
+            <div className="flex justify-between items-center ">
+              <h4 className="text-[rgb(197,206,210)] text-[19px] font-normal">
+                Eng yuqori harorat
+              </h4>
+              <div className="flex items-center gap-[30px]">
+                <p className="text-[19px] text-[#FFF]">19 °C</p>
+                <img className={imgWith} src={maxTemp} alt="" />
               </div>
             </div>
-            <div>
-              <h4>Joriy harorat</h4>
-              <div>
-                <p>19 °C</p>
-                <img src={minTemp} alt="" />
+            <div className={divStyle}>
+              <h4 className={h429}>Eng past harorat</h4>
+              <div className={divStyle2}>
+                <p className={p29}>19 °C</p>
+                <img className={imgWith} src={minTemp} alt="" />
               </div>
             </div>
-            <div>
-              <h4>Joriy harorat</h4>
-              <div>
-                <p>19 °C</p>
-                <img src={normaly} alt="" />
+            <div className={divStyle}>
+              <h4 className={h429}>Namlik</h4>
+              <div className={divStyle2}>
+                <p className={p29}>19 °C</p>
+                <img className={imgWith} src={normaly} alt="" />
               </div>
             </div>
-            <div>
-              <h4>Joriy harorat</h4>
-              <div>
-                <p>19 °C</p>
-                <img src={cloudy11} alt="" />
+            <div className={divStyle}>
+              <h4 className={h429}>Bulutlik</h4>
+              <div className={divStyle2}>
+                <p className={p29}>19 °C</p>
+                <img className={imgWith} src={cloudy11} alt="" />
               </div>
             </div>
-            <div>
-              <h4>Joriy harorat</h4>
-              <div>
-                <p>19 °C</p>
-                <img src={windy} alt="" />
+            <div className={divStyle}>
+              <h4 className={h429}>Shamollik</h4>
+              <div className={divStyle2}>
+                <p className={p29}>19 °C</p>
+                <img className={imgWith} src={windy} alt="" />
               </div>
             </div>
           </div>
